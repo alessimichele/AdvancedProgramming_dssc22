499504f8a2b621ee2c98e471bc80828448de4d4b
alessimichele <alessimichele00@yahoo.it>
2023-02-13 18:21:53 +0100

    ex3py

--- python/game_of_life/main.py
+++ python/game_of_life/main.py
@@ -1,10 +1,21 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
-from game import find_neigh, next_iteration, game_of_life
+from game import game_of_life
 
+"""
+Instruction:
+- be sure to be in the game_of_life directory
 
-### input/putput
+- if you want to change the grid size, change only N_horizontal and N_vertical
+
+- to play, run this code. A window will open. By click on that, you can buld the initial configuration as you want
+
+- when you are done with your configuration, press "q" on your keyboard
+
+- in "images" folder, you should be able to see your output gif, named "game_of_life_{current_time}"
+
+"""
 
 def onclick(event):
     global data
@@ -19,8 +30,10 @@ def onkey(event):
         plt.close()
 
 # Define the grid size
+N_horizontal = 30
+N_vertical = 30
 
-grid_size = (30, 30)
+grid_size = (N_horizontal, N_vertical) 
 data = np.zeros(grid_size, dtype=int)
 
 # Display the grid
